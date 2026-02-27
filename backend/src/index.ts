da63080bf88f1226cb781830fc547ad590b2b8c7
ParthaPradeepNath <parthanath96134@gmail.com>
2026-02-27 18:55:54 +0530

    project setup & cors configured

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+
+import {ENV}  from './config/env';
+import { clerkMiddleware } from '@clerk/express';
+
+
+const app = express();
+
+app.use(cors({ origin: ENV.FRONTEND_URL }));
+app.use(clerkMiddleware()); // auth obj will be attached to the request object
+app.use(express.json()); // parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // parses from data (like HTML forms).
+
+app.get('/', (req, res) => {
+
+    res.json({ 
+        message: "Welcome to SellIt API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        }
+     })
+})
+
+app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT))
