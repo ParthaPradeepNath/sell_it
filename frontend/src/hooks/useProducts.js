dd3156b12aeef6242a97f25cd20a6489ba8f594e
ParthaPradeepNath <parthanath96134@gmail.com>
2026-03-04 11:24:37 +0530

    feat: enhance product management with comments functionality; add useComments hook and CommentsSection component

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,11 +1,44 @@
-import { useMutation, useQuery } from "@tanstack/react-query"
-import { createProduct, getAllProducts } from "../lib/api"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getMyProducts,
+  getProductById,
+} from "../lib/api";
 
 export const useProducts = () => {
-    const result = useQuery({ queryKey: ["products"], queryFn: getAllProducts });
-    return result;
+  const result = useQuery({ queryKey: ["products"], queryFn: getAllProducts });
+  return result;
 };
 
 export const useCreateProduct = () => {
-    return useMutation({ mutationFn: createProduct })
-}
+  return useMutation({ mutationFn: createProduct });
+};
+
+export const useProduct = (id) => {
+  return useQuery({
+    queryKey: ["product", id],
+    queryFn: () => getProductById(id),
+    enabled: !!id, // converting the string to boolean this is called double bang operator in javascript
+  });
+};
+
+export const useDeleteProduct = () => {
+  const queryclient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteProduct,
+
+    onSuccess: () => {
+      queryclient.invalidateQueries({ queryKey: ["myProducts"] });
+    },
+  });
+};
+
+export const useMyProducts = () => {
+  return useQuery({
+    queryKey: ["myProducts"],
+    queryFn: getMyProducts,
+  });
+};
